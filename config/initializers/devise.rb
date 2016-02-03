b4d5098a50ea632a2f51b9aaa69011a56027b3e0
Artem Pisarev <tema77@gmail.com>
2016-02-03 22:24:05 +0600

    sets application layout for profile edit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? 'application' : 'devise' }
 end
