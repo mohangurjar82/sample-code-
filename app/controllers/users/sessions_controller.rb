9226d065723beefa5362153626bde78471d7e67e
Artem Pisarev <tema77@gmail.com>
2016-02-05 04:20:31 +0600

    removes mpx admin auth

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,42 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 
   def create
-    user = User.find_by(email: user_params[:email])
-    if user.nil? || user.mpx_token.blank?
-      user = create_user(user || User.new(user_params))
+    super do |resource|
     end
-    
-    if user.present?
-      if user.valid_password?(user_params[:password]) || create_user(user)
-        sign_in(user)
-        redirect_to after_sign_in_path_for(user), notice: 'Signed in'
-        return  
-      end
-    end
-
-    flash[:alert] = 'Wrong email or password.'
-    redirect_to new_user_session_path
-  end
-
-  private
-
-  def create_user(user)
-    token = get_mpx_token
-    if token['signInResponse']
-      user.mpx_token = token['signInResponse']['token']
-      user.mpx_user_id = token['signInResponse']['userId']
-      user.password_confirmation = user_params[:password] unless user.id
-      user.save ? user : false
-    else
-      false
-    end
-  end
-
-  def get_mpx_token
-    MPX::Account.identity(user_params)
-  end
-
-  def user_params
-    params.require(:user).permit(:email, :password, :remember_me)
   end
 end
