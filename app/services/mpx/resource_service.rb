e4e78266c060ce1a760f032afa21d1144f299a01
Alexander D <alex.hh@me.com>
2016-03-04 19:30:15 +0200

    decrease cache expiration time to 5 minutes

--- app/services/mpx/resource_service.rb
+++ app/services/mpx/resource_service.rb
@@ -9,7 +9,7 @@ class MPX::ResourceService
   def fetch(options = {})
     options.merge! form: 'json', schema: @schema, token: @token
     full_url = "#{@endpoint}?" + options.to_query
-    response = Rails.cache.fetch(full_url, expires: 15.minutes) do
+    response = Rails.cache.fetch(full_url, expires: 5.minutes) do
       HTTParty.get(full_url).body
     end
     Oj.load(response)
