c65fd85748fb95a2c378b4f686d3fab9022c7a9e
Alexander D <alex.hh@me.com>
2016-03-04 19:59:46 +0200

    do not show media for user without orders

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,6 +1,11 @@
 class MediaController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def show
+    unless current_user.orders.any?
+      redirect_to products_path
+      return
+    end
     @mobile = request.user_agent. =~ /mobile/i
     @media = MPX::Media.find_by_number(params[:number])
   end
