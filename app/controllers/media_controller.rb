2e64026d2abff5bd433b88a3fbe2af8eaf2fb6e4
Alexander D <alex.hh@me.com>
2016-03-06 19:54:49 +0200

    Revert "do not show media for user without orders"
    This reverts commit c65fd85748fb95a2c378b4f686d3fab9022c7a9e.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,11 +1,6 @@
 class MediaController < ApplicationController
-  before_action :authenticate_user!
-
+  
   def show
-    unless current_user.orders.any?
-      redirect_to products_path
-      return
-    end
     @mobile = request.user_agent. =~ /mobile/i
     @media = MPX::Media.find_by_number(params[:number])
   end
