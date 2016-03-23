35d7aac188e72690a4a7c4602c6ac0e7f3a49f35
Peter Tracey <peter@petetracey.com>
2016-03-23 10:01:54 -0400

    moving awe and oan to top

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,8 @@ class CategoriesController < ApplicationController
   def show
     @category = MPX::Category.find_by_number(params[:id])
     @media = @category.media(page: params[:page])
+    @oan = MPX::Media.find_by_number(1428037766)
+    @awe = MPX::Media.find_by_number(147013915)
   end
 
   def music
