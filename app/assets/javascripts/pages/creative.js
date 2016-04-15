c100c9dd0c7f30482a78097f29783b1ed28eea14
Peter Tracey <peter@petetracey.com>
2016-04-15 05:26:39 -0400

    updating 6 image area

--- app/assets/javascripts/pages/creative.js
+++ app/assets/javascripts/pages/creative.js
@@ -45,4 +45,28 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+
+    $(function () {
+      var rotateInterval;
+      $(".hoverimage").hover(
+        function () {
+          var img = $(this);
+          var parts = img.attr("src").split(".");
+          var imgIndex = 1;
+
+          rotateInterval = window.setInterval(function () {
+            img.attr("src", parts[0] + "-" + imgIndex + ".jpg");
+            imgIndex++;
+            if (imgIndex > 3) {
+              imgIndex = 1;
+            }
+          }, 2000);
+        },
+        function () {
+          window.clearInterval(rotateInterval);
+          var base = $(this).attr("src").split("-")[0].split(".")[0];
+          $(this).attr("src", base + ".png");
+        });
+    });
+
 })(jQuery); // End of use strict
