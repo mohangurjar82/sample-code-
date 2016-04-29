e61fae19e6f2efc8f956431f92d67b0ff878d635
Alexander D <alex.hh@me.com>
2016-04-30 01:34:49 +0300

    store local files locally

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,8 +7,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :aws
+  if Rails.env.production?
+    storage :aws
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
