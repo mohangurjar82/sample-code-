0e9061376da3a37bbf5c473c9d31848e6bae1756
Alexander D <alex.hh@me.com>
2016-05-04 21:43:07 +0300

    fix products page with new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,10 @@ class ProductsController < ApplicationController
   layout 'new_layout'
   
   def index
-    all_products = Product.all
-    @additional_products, @products = all_products.partition do |p|
-      tags = p.pricing_plan['pricingTiers'][0]['productTags'] rescue []
-      tags.find { |t| t['title'].to_s =~ /A La Carte/i }
-    end
-    @json_products = all_products.map { |p| [p.id, p] }.to_h
+    @products = Product.where(available: true)
+    @additional_products = []
+
+    @json_products = @products.map { |p| [p.id, p.slice(:title, :price)] }.to_h
 
     @skip_javascript = true
   end
