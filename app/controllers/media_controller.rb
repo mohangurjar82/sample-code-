d0be0bfece88ecb752cd8577a45e348aa80875ad
Peter Tracey <peter@petetracey.com>
2016-05-05 10:39:46 -0400

    Updating events

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -7,6 +7,15 @@ class MediaController < ApplicationController
       render 'media/show_book'
     elsif @media.category_name =~ /Games/
       render 'media/show_game'
+    elsif @media.category_name =~ /Events/
+      @results = Events.where("start_date > ? AND media_id = ?", Date.today, @media.number)
+      if @results.nil
+        @hasNextEvent = false
+      else
+        @hasNextEvent = true
+        @nextEvent = @results.first
+      end
+      render 'media/show_event'
     else
       current_user.push_media_to_history(@media.number) if current_user.present?
       render 'media/new/show', layout: 'new_layout'
