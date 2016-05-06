973ef15b37c6163bb3cfdc4b29df3738c7944407
Alexander D <alex.hh@me.com>
2016-05-06 18:27:19 +0300

    created checkout page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504185654) do
+ActiveRecord::Schema.define(version: 20160506152335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,13 +147,13 @@ ActiveRecord::Schema.define(version: 20160504185654) do
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "price"
     t.string   "billing_period"
     t.string   "stripe_id"
     t.integer  "product_id"
     t.string   "stripe_plan_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "pricing_plan_id"
   end
 
   create_table "users", force: :cascade do |t|
