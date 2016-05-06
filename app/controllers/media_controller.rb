c0cdee416e9bed7a19b7137802cebf014dba2410
Peter Tracey <peter@petetracey.com>
2016-05-06 05:39:12 -0400

    Updating events

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -8,15 +8,15 @@ class MediaController < ApplicationController
     elsif @media.category_name =~ /Games/
       render 'media/show_game'
     elsif @media.category_name =~ /Events/
-      @results = Events.where("start_date > ? AND end_date < ? AND media_id = ?", Date.today, Date.today, @media.number)
-      if @results.nil?
+      @results = Events.where("start_date > ? AND end_date < ? AND media_id = ?", Time.now, Time.now, @media.number)
+      if @results.first.nil?
         @hasCurrentEvent = false
       else
         @hasCurrentEvent = true
         @currentEvent = @results.first
       end
-      @results = Events.where("start_date > ? AND media_id = ?", Date.today, @media.number)
-      if @results.nil?
+      @results = Events.where("start_date > ? AND media_id = ?", Time.now, @media.number)
+      if @results.first.nil?
         @hasNextEvent = false
       else
         @hasNextEvent = true
