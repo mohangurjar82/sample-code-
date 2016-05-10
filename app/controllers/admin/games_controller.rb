3e7a36789d9c5b5f6e970a08f9ac4d2632b24667
Alexander D <alex.hh@me.com>
2016-05-11 00:50:46 +0300

    reset admin controllers changes

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -3,19 +3,10 @@ module Admin
     # To customize the behavior of this controller,
     # simply overwrite any of the RESTful actions. For example:
     #
-    def index
-      search_term = params[:search].to_s.strip
-      resources = Administrate::Search.new(resource_resolver, search_term).run
-      resources = order.apply(resources).where(is_a_game: true)
-      resources = resources.page(params[:page]).per(records_per_page)
-      page = Administrate::Page::Collection.new(dashboard, order: order)
-
-      render locals: {
-        resources: resources,
-        search_term: search_term,
-        page: page,
-      }
-    end
+    # def index
+    #   super
+    #   @resources = Category.all.paginate(10, params[:page])
+    # end
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
