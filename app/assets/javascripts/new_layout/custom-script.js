d31abc8d13d2ca8c81cf38114a2b64a33ec02ace
robert <rob@totaldiabetessupply.com>
2016-05-16 20:17:03 -0400

    css img change

--- app/assets/javascripts/new_layout/custom-script.js
+++ app/assets/javascripts/new_layout/custom-script.js
@@ -268,6 +268,27 @@ jQuery(document).ready(function($) {
 
     /* Owl Slider For videos list
     ======================================================*/
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
     if ($('.videos-list').length) {
         $('.videos-list').owlCarousel({
             loop:true,
