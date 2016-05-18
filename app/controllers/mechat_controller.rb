35f2b0f108b8738a5840c32417a7753e0132a66c
robert <rob@totaldiabetessupply.com>
2016-05-18 17:55:40 -0400

    mobile

--- app/controllers/mechat_controller.rb
+++ app/controllers/mechat_controller.rb
@@ -1,12 +1,6 @@
 class MechatController < ApplicationController
-  <<<<<<< HEAD
-
-  layout "new_layout"
-  =======
       before_action :authenticate_user!
   layout 'new_layout'
-
-  >>>>>>> dcff15ce7c75ea9c632208b8c4ffb0404467a2e5
   def index
   end
 end
